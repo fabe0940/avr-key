7c245add95889462a7528257719ba70bfb7478ad
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-02-10 15:25:59 -0800

    added states

--- avr-key.c
+++ avr-key.c
@@ -1,13 +1,16 @@
 #include <inttypes.h>
 #include <stdlib.h>
-#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include "state.h"
 
 uint8_t state;
-uint32_t delay;
 void init(void);
 
 int main(void) {
 	exit(0);
+}
+
+void init(void) {
+	state = STATE_START;
 }
---
+++ state.h
@@ -0,0 +1,11 @@
+#ifndef _STATE_H_
+#define _STATE_H_
+
+#define STATE_START 0
+#define STATE_PRESS_ONE 1
+#define STATE_PRESS_TWO 2
+#define STATE_PRESS_THREE 3
+#define STATE_LOCK 4
+#define STATE_UNLOCK 5
+
+#endif /* _STATE_H_ */
